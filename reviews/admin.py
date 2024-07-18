b82cf9aa36917aa10eea3dbf75bd2c2129d44882
TaeHoon <31303655+xoasml@users.noreply.github.com>
2024-07-19 01:16:19 +0900

    add: apps bookings, reviews, wishlists

---
+++ reviews/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Review
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        "__str__",
+        "payload",
+    )
+
+    list_filter = (
+        "user",
+        "rating"
+    )
