f429a989199cbc9a1e077b6a274b6a63318484c7
TaeHoon <31303655+xoasml@users.noreply.github.com>
2024-07-19 18:46:06 +0900

    add: custom filter for review admin

--- reviews/admin.py
+++ reviews/admin.py
@@ -2,6 +2,49 @@ from django.contrib import admin
 from .models import Review
 
 
+class WordFileter(admin.SimpleListFilter):
+    title = "Filter by words!"
+
+    parameter_name = "word"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("gggg", "gggg"),
+            ("야미", "야미"),
+            ("좋아요", "좋아요"),
+        ]
+
+    def queryset(self, request, reviews):
+        word = self.value()
+        if word:
+            return reviews.filter(payload__contains=word)
+        else :
+            return reviews
+
+
+# 나쁜 리뷰 3점 미만 / 좋은 리뷰 3점 이상 필터 만들기
+
+class UpDownFilter(admin.SimpleListFilter):
+    title = "rating 3up 3down"
+
+    parameter_name = "updown"
+
+    def lookups(self, request, reviews):
+        return [
+            ("up", "3up"),
+            ("down", "3down")
+        ]
+
+    def queryset(self, request, reviews):
+        updown = self.value()
+        if updown == "up":
+            return reviews.filter(rating__gte=3)
+        elif updown == "down":
+            return reviews.filter(rating__lt=3)
+        else:
+            return reviews
+
+
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
     list_display = (
@@ -11,5 +54,10 @@ class ReviewAdmin(admin.ModelAdmin):
 
     list_filter = (
         "user",
-        "rating"
+        "rating",
+        "user__is_host",
+        "room__category",
+        "room__pet_friendly",
+        WordFileter,
+        UpDownFilter,
     )
--- rooms/admin.py
+++ rooms/admin.py
@@ -2,13 +2,23 @@ from django.contrib import admin
 from .models import Room, Amenity
 
 
+@admin.action(description="Set prices to zero")
+def reset_prices(model_admin, request, rooms):
+    for room in rooms.all():
+        room.price = 0
+        room.save()
+
+
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
+    actions = (reset_prices,)
+
     list_display = (
         "name",
         "price",
         "kind",
         "total_amenities",
+        "rating",
         "owner",
         "created_at",
     )
@@ -23,6 +33,10 @@ class RoomAdmin(admin.ModelAdmin):
         "amenities",
         "created_at",
         "updated_at",
+    )
+
+    search_fields = (
+        "owner__username",
     )
 
 
